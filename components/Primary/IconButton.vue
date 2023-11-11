bbd3b6b7b7e5ddb3fbf3447061663946cb5ae10b
ChathuraSudaraka <chathurabro68@gmail.com>
2023-11-11 18:18:44 +0530

    User

---
+++ components/Primary/IconButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <button
+    type="button"
+    class="py-2 px-2 inline-flex text-2xl font-medium rounded-lg border border-gray-200 shadow-sm hover:-translate-y-1 duration-300"
+    :class="colors"
+    @click="method"
+  >
+    <Icon :name="icon" />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    colors: {
+      type: String,
+      default: "bg-gray-800 text-white",
+    },
+    icon: String,
+    method: Function,
+  },
+};
+</script>
+
+<style>
+</style>
--- pages/library/users.vue
+++ pages/library/users.vue
@@ -1,13 +1,59 @@
-<script setup>
+<template>
+  <NuxtLayout name="app">
+    <div class="rounded-lg bg-slate-50 shadow-md">
+      <div class="p-5">
+        <!-- Modal -->
+      </div>
+      <div class="overflow-x-auto shadow-md rounded-b-lg">
+        <table class="w-full text-md text-left text-gray-500">
+          <thead class="text-sm text-gray-700 uppercase bg-gray-50">
+            <tr class="bg-white border-b hover:bg-gray-50">
+              <th scope="col" class="px-6 py-3">ID</th>
+              <th scope="col" class="px-6 py-3">Name</th>
+              <th scope="col" class="px-6 py-3">Book ID</th>
+              <th scope="col" class="px-6 py-3">Role</th>
+              <th scope="col" class="px-6 py-3">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="bg-white border-b hover:bg-gray-50">
+              <td class="px-6 py-4">doe</td>
+              <td class="px-6 py-4">doe</td>
+              <td class="px-6 py-4">doe</td>
+              <td class="px-6 py-4">doe</td>
+              <td class="px-6 py-4 text-right">
+                <!-- Assuming PrimaryIconButton is properly defined -->
+                <PrimaryIconButton
+                  icon="material-symbols-light:edit-square-outline"
+                />
+              </td>
+            </tr>
+            <tr class="bg-white border-b hover:bg-gray-50">
+              <td class="px-6 py-4">doe</td>
+              <td class="px-6 py-4">doe</td>
+              <td class="px-6 py-4">doe</td>
+              <td class="px-6 py-4">doe</td>
+              <td class="px-6 py-4 text-right">
+                <!-- Assuming PrimaryIconButton is properly defined -->
+                <PrimaryIconButton
+                  icon="material-symbols-light:edit-square-outline"
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </NuxtLayout>
+</template>
+  
+  <script setup>
 useHead({
-    title: 'Users | Sri Dharmaloka College',
+  title: "Users | Sri Dharmaloka College",
 });
 </script>
-
-<template>
-    <NuxtLayout name="app">
-        <div class="rounded-lg bg-slate-50 p-5 shadow-md">
-            <!-- Code Here -->
-        </div>
-    </NuxtLayout>
-</template>
+  
+  <style>
+/* Add your styles here if needed */
+</style>
+  
